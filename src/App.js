f2ddeef975b2653f87297b123ed3fc7b9e998919
andrewzweb <andrewzweb@gmail.com>
2021-04-23 15:06:41 +0300

    Lasy loading

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { useEffect } from 'react'
 import TodoList from './Todo/TodoList'
 import Context from './context'
-import AddTodo from './Todo/AddTodo'
 import Loader from './Loader'
 
+
+const AddTodo = React.lazy(() => new Promise (resolve => {
+  setTimeout(() => {
+    resolve(import('./Todo/AddTodo'))
+  }, 1500)
+})) 
 
 function App() {
   const [todos, setTodos] = React.useState([])
@@ -52,7 +57,9 @@ function App() {
     <Context.Provider value={{ removeTodo }}>
       <div className="wrapper">
         <h1>Todos: </h1>
-        <AddTodo onCreate={addTodo}/>
+        <React.Suspense fallback={<Loader/>}>
+          <AddTodo onCreate={addTodo}/>
+        </React.Suspense>
 
         { loading && <Loader/>}
         { todos.length ? (
